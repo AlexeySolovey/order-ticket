e35d12d25f9ee4a7f793632fdea155272d750fed
Nikolay Tavolzhanskiy <tavolzahaznskiy@gmail.com>
2020-11-15 21:03:10 +0200

    block eat.

--- order-ticket_files/new-scripts.js
+++ order-ticket_files/new-scripts.js
@@ -3,3 +3,28 @@ $('#additional-services-button').click(function(){
     $('#order-first-step').hide();
     $('#order-second-step').fadeIn('slow');
 });
+
+
+// ============== add order-footer-eat ========
+    let buttonCountMinus = document.getElementById("buttonCountMinus");
+    let count = document.getElementById("buttonCountNumber");
+    let count2 = document.getElementById("num");
+    let number = 1;
+    let price = 485;
+
+    buttonCountPlus.onclick = function() {
+        if (number <= 9 ) {
+            number++;
+            count.innerHTML = number;
+            count2.value = number * price;
+        }
+    };
+
+    buttonCountMinus.onclick = function() {
+    if (number >= 2) {
+        number--;
+        count.innerHTML = number;
+        count2.value = number * price;
+        }
+    }
+// ============================================
