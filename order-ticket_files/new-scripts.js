1d188a885047b132120a7dad4c3316c1f82ee8aa
Nikolay Tavolzhanskiy <tavolzahaznskiy@gmail.com>
2020-11-22 08:12:55 +0200

    work with left panel and work with input and id.

--- order-ticket_files/new-scripts.js
+++ order-ticket_files/new-scripts.js
@@ -6,25 +6,26 @@ $('#additional-services-button').click(function(){
 
 
 // ============== add order-footer-eat ========
-    let buttonCountMinus = document.getElementById("buttonCountMinus");
-    let count = document.getElementById("buttonCountNumber");
-    let count2 = document.getElementById("num");
-    let number = 1;
-    let price = 485;
+let buttonCountMinus = document.getElementById("minus");
+let count = document.getElementById("buttonCountNumber");
+let count2 = document.getElementById("num");
+let number = 1;
+let price = 485;
 
-    buttonCountPlus.onclick = function() {
-        if (number <= 9 ) {
-            number++;
-            count.innerHTML = number;
-            count2.value = number * price;
-        }
-    };
+plus.onclick = function() {
+  if (number <= 9 ) {
+    number++;
+    count.innerHTML = number;
+    count2.value = number * price;
+  }
+};
 
-    buttonCountMinus.onclick = function() {
-    if (number >= 2) {
-        number--;
-        count.innerHTML = number;
-        count2.value = number * price;
-        }
-    }
+minus.onclick = function() {
+if (number >= 2) {
+    number--;
+    count.innerHTML = number;
+    count2.value = number * price;
+  }
+}
 // ============================================
+
